da6c16327f73d8c1c5977aebf20933ac1b988244
Ben-init <lopezpetroni@outlook.com>
2022-09-30 20:34:39 -0300

    [fix] error message

--- network/response.js
+++ network/response.js
@@ -1,15 +1,14 @@
 const success = (req, res, message, status) => {
     res.status(status || 200).send({
-        error: '',
+        error: 'null',
         body : message,
     });
 }
 
 const error = (req, res, message, status, detail) => {
-    console.error(detail);
     res.status(status || 500).send({
-        error: '',
-        body : message,
+        error: message.message,
+        body : message.stack,
     });
 }
 
