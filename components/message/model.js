2eae81423c97aad76cfb4e65f440665ecaf4061e
Ben-init <lopezpetroni@outlook.com>
2022-10-02 20:57:53 -0300

    message model

---
+++ components/message/model.js
@@ -0,0 +1,14 @@
+const { Schema, model } = require('mongoose');
+
+const messageSchema = new Schema({
+    user: {
+        type: String,
+        required: true,
+    },
+    message: String,
+    date: Date,
+});
+
+const Model = model('message', messageSchema);
+
+module.exports = Model;
