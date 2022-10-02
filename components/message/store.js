25be0d7fb3a4805cbc28098d6c58ca52341fe02f
Ben-init <lopezpetroni@outlook.com>
2022-10-02 20:49:31 -0300

    store file (database connection)

---
+++ components/message/store.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Model = require('./model');
+const { config } = require('./../../config');
+
+const user = config.dbUser; 
+const password = config.dbPassword;
+
+console.log('db connected');
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(
+            `mongodb+srv://${user}:${password}@cluster0.1anfhot.mongodb.net/?retryWrites=true&w=majority`
+        );
+        console.log('DB connected!!');
+    } catch (err) {
+        console.log('Failed to connect to DB', err);
+    }
+};
+
+connectDB();
+
+async function addMessage(message) {
+    const myMessage = new Model(message);
+    await myMessage.save();
+}
+
+ async function getMessage(id) {
+    const message = await Model.findById(id);
+    return message;
+}
+
+async function allMessage() {
+    const messages = await Model.find();
+    return messages;
+}
+
+module.exports = {
+    add: addMessage,
+    get: getMessage,
+    getAll: allMessage,
+}
