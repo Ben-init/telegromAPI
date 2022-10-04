6848ea02f9bc2661192a83e20547324133ff9be5
Ben-init <lopezpetroni@outlook.com>
2022-10-04 17:40:04 -0300

    [update] update and delete function (added)

--- components/message/controller.js
+++ components/message/controller.js
@@ -16,18 +16,56 @@ async function addMessage(user, message) {
 }
 
 async function getMessage(id) {
-
     const message = await store.get(id);
     return message;
 }
 
-async function getAllMessage() {
-    const messages = await store.getAll();
+async function getAllMessage(filterUser) {
+    const filter = {};
+    if (filterUser) {
+        filter.user = filterUser;
+    }
+    const messages = await store.getAll(filter);
     return messages;
+}
+
+async function updateMessage(id, text) {
+    try {
+
+        if (!text || !id) {
+            throw new Error('[Controller] invalid data')
+        }
+        
+        const message = await store.get(id);
+        message.message = text;
+        message.modified = new Date();
+        const updatedMessage = await store.update(message);
+        return updatedMessage;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+async function deleteMessage(id) {
+    try {
+
+        const messageIsTrue = await store.exists(id);
+        console.log(messageIsTrue);
+        if (!id || !messageIsTrue) {
+            console.log('hola');
+            throw new Error('[Controller] invalid or missing params')
+        }
+        const deletedMessage = await store.remove(id);
+        return deletedMessage;
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 module.exports = {
     addMessage,
     getMessage,
-    getAllMessage
+    getAllMessage,
+    updateMessage,
+    deleteMessage
 }
