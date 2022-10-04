ee0b20534ebff8cfcbff7c51f6ffdce455097adb
Ben-init <lopezpetroni@outlook.com>
2022-10-04 18:24:48 -0300

    [update] database connection

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 
 const Routes = require('./network/routes');
+const ConnectDB = require('./db');
 
 const app = express();
 const port = 3000;
 
+ConnectDB();
 app.use(express.json());
 
 Routes(app);
