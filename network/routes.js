e4e4e8efc0f85064c31a237012624bb328eafaf6
Ben-init <lopezpetroni@outlook.com>
2022-10-05 20:50:52 -0300

    [update] /user (api)

--- network/routes.js
+++ network/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 
 const messageRouter = require('./../components/message/network'); 
+const userRouter = require('./../components/user/network'); 
 
 const Routes = (server) => {
     const router = express.Router();
     server.use('/api/', router);
     router.use('/message', messageRouter);
+    router.use('/user', userRouter);
 }
 
 module.exports = Routes;
