5b2580d448e2754e0a24cacd12c087895dc364bb
Ben-init <lopezpetroni@outlook.com>
2022-10-07 19:28:32 -0300

    [update] chat routes

--- network/routes.js
+++ network/routes.js
@@ -2,12 +2,14 @@ const express = require('express');
 
 const messageRouter = require('./../components/message/network'); 
 const userRouter = require('./../components/user/network'); 
+const chatRouter = require('./../components/chat/network'); 
 
 const Routes = (server) => {
     const router = express.Router();
     server.use('/api/', router);
     router.use('/message', messageRouter);
     router.use('/user', userRouter);
+    router.use('/chat', chatRouter);
 }
 
 module.exports = Routes;
