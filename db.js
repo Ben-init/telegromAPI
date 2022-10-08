1c2928ce6621e2ff2c099d97a183d46c85375caa
Ben-init <lopezpetroni@outlook.com>
2022-10-08 20:27:53 -0300

    [update] env config (dbURL)

--- db.js
+++ db.js
@@ -2,14 +2,9 @@ const mongoose = require('mongoose');
 
 const { config } = require('./config');
 
-const user = config.dbUser; 
-const password = config.dbPassword;
-
 const ConnectDB = async () => {
     try {
-        await mongoose.connect(
-            `mongodb+srv://${user}:${password}@cluster0.1anfhot.mongodb.net/?retryWrites=true&w=majority`
-        );
+        await mongoose.connect(config.dbUrl);
         console.log('DB connected!!');
     } catch (err) {
         console.log('Failed to connect to DB', err);
